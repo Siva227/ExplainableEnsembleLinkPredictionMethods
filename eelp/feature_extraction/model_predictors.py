82e0aef5b1b716eb10d1d8f591b61e46545da3c6
Siva Sankar E.V.P <sankaremany@gmail.com>
2023-05-08 01:14:16 -0400

    Changes to improve computation speed

--- eelp/feature_extraction/model_predictors.py
+++ eelp/feature_extraction/model_predictors.py
@@ -29,6 +29,7 @@ class LouvainScorer(GraphScorer):
         self.randomize = randomize
         self.random_state = random_state
         self.partition = None
+        self.base_modularity = None
 
     def fit(self, X, y=None):
         self.partition = community_louvain.best_partition(
@@ -39,18 +40,28 @@ class LouvainScorer(GraphScorer):
             self.randomize,
             self.random_state,
         )
+        self.base_modularity = community_louvain.modularity(self.partition, self.input_network)
         return self
 
     def transform(self, X):
         X = self.make_dataset(X)
         score = []
         for e_pair in X.itertuples(name=None, index=False):
-            in_copy = self.input_network.copy()
-            if e_pair in in_copy.edges:
-                in_copy.remove_edge(*e_pair)
-            q_wo_edge = community_louvain.modularity(self.partition, in_copy)
-            in_copy.add_edge(*e_pair)
-            q_w_edge = community_louvain.modularity(self.partition, in_copy)
+            # Commenting out the below code in the interest of efficiency.
+            # Assumption is that the edges being scored are not part of the initial network
+            # Which is true for our analysis.
+
+            # in_copy = self.input_network.copy()
+            # if e_pair in in_copy.edges:
+            #     in_copy.remove_edge(*e_pair)
+            # q_wo_edge = community_louvain.modularity(self.partition, in_copy)
+            # in_copy.add_edge(*e_pair)
+            # q_w_edge = community_louvain.modularity(self.partition, in_copy)
+            # score.append(q_w_edge - q_wo_edge)
+            q_wo_edge = self.base_modularity
+            self.input_network.add_edge(*e_pair)
+            q_w_edge = community_louvain.modularity(self.partition, self.input_network)
+            self.input_network.remove_edge(*e_pair)
             score.append(q_w_edge - q_wo_edge)
         return np.array(score).reshape(-1, 1)
 
@@ -73,18 +84,24 @@ class InfomapScorer(GraphScorer):
         X = self.make_dataset(X)
         im_score = []
         for e_pair in X.itertuples(name=None, index=False):
-            if e_pair in self.input_network.edges:
-                self.im.remove_link(*e_pair)
-                self.im.run(initial_partition=self.im_modules, no_infomap=True)
-                im_wo_edge = self.im.codelength
-            else:
-                im_wo_edge = self.im_code_length
+            # Commenting out the below code in the interest of efficiency.
+            # Assumption is that the edges being scored are not part of the initial network
+            # Which is true for our analysis.
+
+            # if e_pair in self.input_network.edges:
+            #     self.im.remove_link(*e_pair)
+            #     self.im.run(initial_partition=self.im_modules, no_infomap=True)
+            #     im_wo_edge = self.im.codelength
+            # else:
+            #     im_wo_edge = self.im_code_length
+            im_wo_edge = self.im_code_length
             self.im.add_link(*e_pair)
             self.im.run(initial_partition=self.im_modules, no_infomap=True)
             im_w_edge = self.im.codelength
             im_score.append(im_wo_edge - im_w_edge)
-            if e_pair not in self.input_network.edges:
-                self.im.remove_link(*e_pair)
+            self.im.remove_link(*e_pair)
+            # if e_pair not in self.input_network.edges:
+            #     self.im.remove_link(*e_pair)
         return np.array(im_score).reshape(-1, 1)
 
 
