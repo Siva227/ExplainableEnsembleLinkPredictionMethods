81867d017558647f2f50016bbb9dc2742cc62157
Siva Sankar E.V.P <sankaremany@gmail.com>
2023-05-08 12:21:04 -0400

    Ensure fitted variables naming convention by adding underscore at the end

--- eelp/feature_extraction/model_predictors.py
+++ eelp/feature_extraction/model_predictors.py
@@ -28,11 +28,11 @@ class LouvainScorer(GraphScorer):
         self.resolution = resolution
         self.randomize = randomize
         self.random_state = random_state
-        self.partition = None
-        self.base_modularity = None
+        self.best_partition_ = None
+        self.base_modularity_ = None
 
     def fit(self, X, y=None):
-        self.partition = community_louvain.best_partition(
+        self.best_partition_ = community_louvain.best_partition(
             self.input_network,
             self.partition,
             self.weight,
@@ -40,7 +40,7 @@ class LouvainScorer(GraphScorer):
             self.randomize,
             self.random_state,
         )
-        self.base_modularity = community_louvain.modularity(self.partition, self.input_network)
+        self.base_modularity_ = community_louvain.modularity(self.best_partition_, self.input_network)
         return self
 
     def transform(self, X):
@@ -58,9 +58,9 @@ class LouvainScorer(GraphScorer):
             # in_copy.add_edge(*e_pair)
             # q_w_edge = community_louvain.modularity(self.partition, in_copy)
             # score.append(q_w_edge - q_wo_edge)
-            q_wo_edge = self.base_modularity
+            q_wo_edge = self.base_modularity_
             self.input_network.add_edge(*e_pair)
-            q_w_edge = community_louvain.modularity(self.partition, self.input_network)
+            q_w_edge = community_louvain.modularity(self.best_partition_, self.input_network)
             self.input_network.remove_edge(*e_pair)
             score.append(q_w_edge - q_wo_edge)
         return np.array(score).reshape(-1, 1)
@@ -69,15 +69,23 @@ class LouvainScorer(GraphScorer):
 class InfomapScorer(GraphScorer):
     def __init__(self, input_network, args=None, two_level=True, num_trials=1):
         super(InfomapScorer, self).__init__(input_network)
-        self.im = Infomap(args=args, two_level=two_level, silent=True, num_trials=num_trials)
-        self.im.add_networkx_graph(self.input_network)
-        self.im_modules = None
-        self.im_code_length = None
+        self.args = args
+        self.two_level = two_level
+        self.num_trials = num_trials
+        self.im_ = None
+        # self.im = Infomap(args=args, two_level=two_level, silent=True, num_trials=num_trials)
+        # self.im.add_networkx_graph(self.input_network)
+        self.im_modules_ = None
+        self.im_code_length_ = None
 
     def fit(self, X, y=None):
-        self.im.run()
-        self.im_modules = self.im.get_modules()
-        self.im_code_length = self.im.codelength
+        self.im_ = Infomap(
+            args=self.args, two_level=self.two_level, silent=True, num_trials=self.num_trials
+        )
+        self.im_.add_networkx_graph(self.input_network)
+        self.im_.run()
+        self.im_modules_ = self.im_.get_modules()
+        self.im_code_length_ = self.im_.codelength
         return self
 
     def transform(self, X):
@@ -94,12 +102,12 @@ class InfomapScorer(GraphScorer):
             #     im_wo_edge = self.im.codelength
             # else:
             #     im_wo_edge = self.im_code_length
-            im_wo_edge = self.im_code_length
-            self.im.add_link(*e_pair)
-            self.im.run(initial_partition=self.im_modules, no_infomap=True)
-            im_w_edge = self.im.codelength
+            im_wo_edge = self.im_code_length_
+            self.im_.add_link(*e_pair)
+            self.im_.run(initial_partition=self.im_modules_, no_infomap=True)
+            im_w_edge = self.im_.codelength
             im_score.append(im_wo_edge - im_w_edge)
-            self.im.remove_link(*e_pair)
+            self.im_.remove_link(*e_pair)
             # if e_pair not in self.input_network.edges:
             #     self.im.remove_link(*e_pair)
         return np.array(im_score).reshape(-1, 1)
@@ -110,19 +118,19 @@ class MDLScorer(GraphScorer):
         super(MDLScorer, self).__init__(input_network)
         self.gt_in = nx2gt(input_network)
         self.deg_corr = deg_corr
-        self.block_state = None
-        self.base_entropy = None
+        self.block_state_ = None
+        self.base_entropy_ = None
 
     def fit(self, X, y=None):
-        self.block_state = minimize_blockmodel_dl(
+        self.block_state_ = minimize_blockmodel_dl(
             self.gt_in, state_args=dict(deg_corr=self.deg_corr)
         )
-        self.base_entropy = self.block_state.entropy()
+        self.base_entropy_ = self.block_state_.entropy()
         return self
 
     def transform(self, X):
         X = self.make_dataset(X)
         dl_score = [
-            self.block_state.get_edges_prob([i]) for i in X.itertuples(name=None, index=False)
+            self.block_state_.get_edges_prob([i]) for i in X.itertuples(name=None, index=False)
         ]
         return np.array(dl_score).reshape(-1, 1)
